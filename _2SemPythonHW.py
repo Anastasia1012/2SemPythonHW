98933b858d240b87deb4a39d5524e7abfdce1b21
Anastasia <79032456178@yandex.ru>
2022-09-11 18:31:48 +0300

    add

--- _2SemPythonHW.py
+++ _2SemPythonHW.py
@@ -1,3 +1,4 @@
+import time
 #Специализация Тестировщик
 
 #1 - Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр. 
@@ -8,18 +9,22 @@
 #67.82 -> 23
 #0.56 -> 11
 
-num = input('input number') 
-num=num.replace('.','') #если число дробное делаем функцию с точкой и выделяем её кавычками. Далее ставим кавычки с пробелом,
-#преобразовывая в целое число
-num_abs = abs(num) #Если число отрицательное, это не влияет на сумму его цифр. 
+from sre_parse import DIGITS
+from string import digits
+
+
+num = input('input number and find sum') 
+num=num.replace('.','') #если число дробное функцией replace удаляем точку(меняем . на '') 
+num=int(num) #преобразование в целое число
+num = abs(num) #Если число отрицательное, это не влияет на сумму его цифр. 
 #В таком случае достаточно будет использовать встроенную в Python функции abc(), 
 #которая возвращает абсолютное значение переданного ей аргумента.
-num=int(num)
 sum = 0
 while num != 0:
     sum = sum + num % 10 #23%10=3
     num = num // 10#2
-print(f'sum=: {sum}')#print('sum = ',sum)
+print(f'sum=: {sum}')#print('sum = ',sum) #f,format помогает преобразовывать в текст переменную. 
+#Вместо sum=: можно написать что угодно.Здесь пишем просто текст(результат и тд)
 
 #2 - Напишите программу, которая принимает на вход число N и выдает набор произведений (набор - это список) чисел от 1 до N.
 #Не используйте функцию math.factorial.
@@ -28,23 +33,24 @@ print(f'sum=: {sum}')#print('sum = ',sum)
 #Пример:
 #- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-#Разобрать данный код:
-#def f(n):
-#    return 1 if n == 0 else n * f(n - 1)
 
-n = int(input('Input number n'))
+try: # в этом цикле проверка корректности выполнения кода, в том числе и на буквы
+    n = int(input('Input number n and output mult list'))
  
-factorial = 1
- 
-for i in range(2, n+1): #двойка, потому что единицу мы уже учли
-    factorial *= i # *= Оператор *= сначала умножает значение выражения (справа от оператора)
+    factorial = 1
+    print(factorial)
+    for i in range(2, n+1): #двойка, потому что единицу мы уже учли
+        factorial *= i 
+        print(factorial)
+        # *= Оператор *= сначала умножает значение выражения (справа от оператора)
     #на значение переменной или свойства (слева от оператора). 
     #Затем оператор назначает результат этой операции переменной или свойству.
  
-print(factorial)
+    
+except ValueError:
+    print('error input')
 
-
-n = int(input("Введите число n")) 
+n = int(input("input n")) 
 f = 1
 
 while n > 1:
@@ -52,25 +58,23 @@ while n > 1:
     n = n - 1 # от обратного, так как n у нас больше единицы
 print(f)
 
-n = int(input("Введите число n")) 
+n = int(input("input n")) 
 f = 1
 
 for i in range(2, n+1):
     f = f * i
 print(f)
  
-#можно ли как вариант написать ,что  f это digit, чтобы не делать проверку?
 
 #3.Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 
-n = int(input('Enter number: ')) 
-
-def sequence(n):
-
-    return[round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]   
-        
-print(sequence(n))
-print(round(sum(sequence(n))))
+N = int(input('input number = qty of elements')) 
+a = list() 
+ 
+result = 0 
+for i in range(1, N+1): 
+    result += (1 + 1/i)**i #возведение в степень 1,5*1,5=2,25
+print(round(result, 2))
 
 
 #Специализация Программирование
@@ -81,19 +85,38 @@ print(round(sum(sequence(n))))
 #Это происходит до тех пор, пока не будет найден палиндром.
 #Напишите такую программу, которая найдет палиндром введенного пользователем числа.
 
-#num = 0;
-#pos = 0;
-
-#while num != 0:
+def check_pal(n):
+    res = 0
+    while n != 0:
+        digit=n%10
+        res = res*10+digit 
+        n = n//10
+    return res
     
-#    digit = num % 10 #определить последнюю цифру числа
-    #num = [] #уменьшить число на 1 цифру или задать от нуля до 10
+try:
+    num = int(input('input number and check palindrom'))
+    palindrom = check_pal(num)
+    if num == palindrom:
+        print('input number is palindrom')
+    else:
+        while num != palindrom:
+            num = num + palindrom
+            palindrom=check_pal(num)
+        print(f'output palindrom {num}')
 
-    
-    #new_num = new_num * 10 + digit #увеличить новое число на 1 цифру
+except ValueError:
+    print('error')
+
 
 
 #4 - Реализуйте выдачу случайного числа
 #не использовать random.randint и вообще библиотеку random
 #Можете использовать xor, биты, библиотеку time или datetime (миллисекунды или наносекунды) - для задания случайности
 #Учтите, что есть диапазон: от(минимальное) и до (максимальное)
+n=10
+while n:
+    n-=1#условия выхода из цикла
+    rand_num=int(round(time.time())*1000)# функция времени
+    print(rand_num)
+    #rand_num*=int(rand_num)#выбираем целое значение, умножаем дробное на целое, чтоб избавиться от целой части
+    n=int(input())
